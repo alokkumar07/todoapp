9e19004ec360e5130dbe00147f5b58e39092c9ac
alokkumar07 <aloknith07@gmail.com>
2023-01-15 19:45:53 +0530

    home page implement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
+import Todo from './Todo';
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <>
+    <div className="container">
+        <section className='mt-3 text-center'>
+        <h3>Enter Your Task</h3>
+
+        <div className="todo col-lg-5 mx-auto d-flex justify-content-between align-items-center" >
+            <input  name='task' className='form-control'/>
+            <Button variant='contained'style={{background:"#ee2253"}} className='mx-2'>
+            <AddIcon />
+            </Button>
+        </div>
+        <Todo />
+        </section>
+    </div>
+   </>
   )
 }
 
